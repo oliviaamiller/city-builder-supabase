a8a90fd5811aaa2f27147b9bc082e00385e5ba58
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-04 15:24:33 -0800

    grabbed elements from the dom and console logged them out in js

--- cities/cities.js
+++ cities/cities.js
@@ -1,5 +1,22 @@
 import { checkAuth, logout } from '../fetch-utils.js';
 
+const nameInputEl = document.getElementById('name-input');
+const nameButtonEl = document.getElementById('name-button');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganButtonEl = document.getElementById('slogan-button');
+const waterfrontDropdownEl = document.getElementById('waterfront-dropdown');
+const skylineDropdownEl = document.getElementById('skyline-dropdown');
+const castleDropdownEl = document.getElementById('castle-dropdown');
+const nameOutputEl = document.getElementById('name-output');
+const sloganOutputEl = document.getElementById('slogan-output');
+
+console.log(nameInputEl, nameButtonEl, sloganInputEl, sloganButtonEl, waterfrontDropdownEl, skylineDropdownEl, castleDropdownEl, nameOutputEl, sloganOutputEl);
+
+
+
+
+
+
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
